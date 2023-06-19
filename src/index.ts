ff8f73bcace7300df4328559f5eb73a09e8026e0
Yun <momo1425360@gmail.com>
2023-06-19 20:26:41 +0800

    Remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
@@ -18,8 +17,6 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
-console.log(typeof server);
 app.use('/api/v1/tasks', tasks);
 
 connectDB();
